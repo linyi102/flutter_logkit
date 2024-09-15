36c0758c694877bafd9499775aabe1dbcb69e6d1
linyi <1158029271@qq.com>
2024-09-16 06:27:07 +0800

    feat: get entry button size by globalkey

--- lib/widgets/entry_button.dart
+++ lib/widgets/entry_button.dart
@@ -14,17 +14,18 @@ class _DraggableEntryButtonState extends State<DraggableEntryButton> {
   double dx = 0, dy = 0;
   bool isOpened = false;
 
-  final buttonSize = 36.0;
-  late final halfButtonSize = buttonSize / 2;
+  Size buttonSize = const Size(40, 40);
+  final buttonKey = GlobalKey(debugLabel: 'log_entry_button');
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final screenSize = MediaQuery.of(context).size;
+      buttonSize = buttonKey.currentContext?.size ?? buttonSize;
       setState(() {
-        dx = screenSize.width - 20 - buttonSize;
-        dy = screenSize.height - 20 - buttonSize;
+        dx = screenSize.width - 20 - buttonSize.width;
+        dy = screenSize.height - 20 - buttonSize.height;
       });
     });
   }
@@ -39,6 +40,7 @@ class _DraggableEntryButtonState extends State<DraggableEntryButton> {
       child: Draggable(
         feedback: _buildIcon(),
         child: GestureDetector(
+          key: buttonKey,
           onTap: () async {
             setState(() {
               isOpened = true;
@@ -49,15 +51,19 @@ class _DraggableEntryButtonState extends State<DraggableEntryButton> {
               isOpened = false;
             });
           },
-          child: _buildIcon(),
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: _buildIcon(),
+          ),
         ),
         onDragUpdate: (details) {
           final screenSize = MediaQuery.of(context).size;
+          final halfButtonSize = buttonSize / 2;
           setState(() {
-            dx = (dx + details.delta.dx)
-                .clamp(-halfButtonSize, screenSize.width - halfButtonSize);
-            dy = (dy + details.delta.dy)
-                .clamp(-halfButtonSize, screenSize.height - halfButtonSize);
+            dx = (dx + details.delta.dx).clamp(
+                -halfButtonSize.width, screenSize.width - halfButtonSize.width);
+            dy = (dy + details.delta.dy).clamp(-halfButtonSize.height,
+                screenSize.height - halfButtonSize.height);
           });
         },
       ),
@@ -65,17 +71,13 @@ class _DraggableEntryButtonState extends State<DraggableEntryButton> {
   }
 
   Widget _buildIcon() {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: Container(
-        height: buttonSize,
-        width: buttonSize,
-        decoration: BoxDecoration(
-          color: Colors.black.withOpacity(0.04),
-          shape: BoxShape.circle,
-        ),
-        child: const Icon(Icons.bug_report),
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.04),
+        shape: BoxShape.circle,
       ),
+      child: const Icon(Icons.bug_report),
     );
   }
 }
