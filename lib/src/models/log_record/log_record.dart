442c7dc4d5b3ea449884063a8b642d6b660e976d
linyi <1158029271@qq.com>
2024-09-19 17:51:41 +0800

    refactor: move *LogRecord from models/ to models/log_record/

---
+++ lib/src/models/log_record/log_record.dart
@@ -0,0 +1,3 @@
+export 'http_log_record.dart';
+export 'route_log_record.dart';
+export 'simple_log_record.dart';
--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,9 +1,7 @@
-export 'http_log_record.dart';
 export 'log_level.dart';
 export 'log_record.dart';
+export 'log_record/log_record.dart';
 export 'log_record_filter.dart';
 export 'log_record_type.dart';
 export 'log_settings.dart';
 export 'logkit_settings.dart';
-export 'route_log_record.dart';
-export 'simple_log_record.dart';
