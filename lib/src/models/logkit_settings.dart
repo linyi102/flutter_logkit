243b123cdd6afe5b5d26413753247b45ebb6d914
linyi <1158029271@qq.com>
2024-09-23 01:19:44 +0800

    faet: custom entry icon and offset

--- lib/src/models/logkit_settings.dart
+++ lib/src/models/logkit_settings.dart
@@ -1,14 +1,22 @@
-import 'package:flutter_logkit/src/models/log_settings.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_logkit/logkit.dart';
+
+typedef EntryIconOffsetFunction = Offset Function(
+    Size screenSize, Size buttonSize);
 
 class LogkitSettings extends LogSettings {
   final int? maxLogCount;
   final bool disableAttachOverlay;
   final bool disableRecordLog;
+  final Widget Function()? entryIconBuilder;
+  final EntryIconOffsetFunction? entryIconOffset;
 
   const LogkitSettings({
     this.maxLogCount = 1000,
     this.disableAttachOverlay = false,
     this.disableRecordLog = false,
+    this.entryIconBuilder,
+    this.entryIconOffset,
     super.printToConsole,
     super.printTime,
   });
--- lib/src/widgets/entry_button.dart
+++ lib/src/widgets/entry_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_logkit/src/logger.dart';
+import 'package:flutter_logkit/logkit.dart';
 import 'package:flutter_logkit/src/pages/log_page.dart';
 
 class DraggableEntryButton extends StatefulWidget {
@@ -11,9 +11,8 @@ class DraggableEntryButton extends StatefulWidget {
 }
 
 class _DraggableEntryButtonState extends State<DraggableEntryButton> {
-  double dx = 0, dy = 0;
   bool isOpened = false;
-
+  Offset? buttonOffset;
   Size buttonSize = const Size(40, 40);
   final buttonKey = GlobalKey(debugLabel: 'log_entry_button');
 
@@ -23,54 +22,79 @@ class _DraggableEntryButtonState extends State<DraggableEntryButton> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final screenSize = MediaQuery.of(context).size;
       buttonSize = buttonKey.currentContext?.size ?? buttonSize;
+
+      final calOffset =
+          widget.logger.logkitSettings.entryIconOffset ?? _calInitialIconOffset;
       setState(() {
-        dx = screenSize.width - 20 - buttonSize.width;
-        dy = screenSize.height - 20 - buttonSize.height;
+        buttonOffset = calOffset(screenSize, buttonSize);
       });
     });
   }
 
+  Offset _calInitialIconOffset(Size screenSize, Size buttonSize) {
+    return Offset(
+      screenSize.width - 20 - buttonSize.width,
+      screenSize.height - 20 - buttonSize.height,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (isOpened) return const SizedBox();
+    if (buttonOffset == null) return const SizedBox();
 
     return Positioned(
-      left: dx,
-      top: dy,
-      child: Draggable(
-        feedback: _buildIcon(),
-        child: GestureDetector(
-          key: buttonKey,
-          onTap: () async {
-            setState(() {
-              isOpened = true;
-            });
-            await Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => LogPage(logger: widget.logger)));
-            setState(() {
-              isOpened = false;
-            });
-          },
-          child: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: _buildIcon(),
-          ),
-        ),
-        onDragUpdate: (details) {
-          final screenSize = MediaQuery.of(context).size;
-          final halfButtonSize = buttonSize / 2;
-          setState(() {
-            dx = (dx + details.delta.dx).clamp(
-                -halfButtonSize.width, screenSize.width - halfButtonSize.width);
-            dy = (dy + details.delta.dy).clamp(-halfButtonSize.height,
-                screenSize.height - halfButtonSize.height);
-          });
-        },
+      left: buttonOffset?.dx,
+      top: buttonOffset?.dy,
+      child: AnimatedScale(
+        scale: isOpened ? 0 : 1,
+        curve: Curves.easeOutCubic,
+        duration: const Duration(milliseconds: 300),
+        child: _buildDraggableIcon(context),
       ),
     );
   }
 
+  Widget _buildDraggableIcon(BuildContext context) {
+    return Draggable(
+      feedback: _buildIcon(),
+      child: GestureDetector(
+        key: buttonKey,
+        onTap: _toLogPage,
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: _buildIcon(),
+        ),
+      ),
+      onDragUpdate: (details) {
+        final screenSize = MediaQuery.of(context).size;
+        final halfButtonSize = buttonSize / 2;
+        final dx = buttonOffset?.dx ?? 0, dy = buttonOffset?.dy ?? 0;
+        final newDx = (dx + details.delta.dx).clamp(
+            -halfButtonSize.width, screenSize.width - halfButtonSize.width);
+        final newDy = (dy + details.delta.dy).clamp(
+            -halfButtonSize.height, screenSize.height - halfButtonSize.height);
+        setState(() {
+          buttonOffset = Offset(newDx, newDy);
+        });
+      },
+    );
+  }
+
+  Future<void> _toLogPage() async {
+    setState(() {
+      isOpened = true;
+    });
+    await Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => LogPage(logger: widget.logger)));
+    setState(() {
+      isOpened = false;
+    });
+  }
+
   Widget _buildIcon() {
+    final icon = widget.logger.logkitSettings.entryIconBuilder?.call();
+    if (icon != null) return icon;
+
     return Container(
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
