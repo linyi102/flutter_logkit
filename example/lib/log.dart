b4580765a552e4f6395caece9bf72a8cb8469ff4
linyi <1158029271@qq.com>
2024-09-22 10:50:22 +0800

    feat: enable log in release version via environment

--- example/lib/log.dart
+++ example/lib/log.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_logkit/logkit.dart';
+
+/// Enable log in release version:
+/// flutter build xxx --dart-define logging=true
+const _enableLog = kDebugMode || bool.fromEnvironment('logging');
 
 final logger = LogkitLogger(
   logkitSettings: const LogkitSettings(
+    disableAttachOverlay: !_enableLog,
+    disableRecordLog: !_enableLog,
     printToConsole: true,
     printTime: true,
     maxLogCount: 100,
